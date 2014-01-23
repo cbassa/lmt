c86fbf4842906e5f4bc605a75ad4019cb159df66
Cees Bassa <cbassa@gmail.com>
2014-01-23 15:56:07 +0100

    Initial version of dada reader

---
+++ reader.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <getopt.h>
+#include <stdint.h>
+#include "dada.h"
+
+#define LIM 256
+
+int main(int argc,char *argv[])
+{
+  int arg=0;
+  FILE *infile,*outfile;
+  char infname[LIM],outfname[LIM];
+  char *buffer;
+  struct dada_header hdr;
+  unsigned int bytes_read,blocksize=64000;
+
+  // Decode options
+  while ((arg=getopt(argc,argv,"i:o:b:"))!=-1) {
+    switch (arg) {
+
+    case 'i':
+      strcpy(infname,optarg);
+      break;
+
+    case 'o':
+      strcpy(outfname,optarg);
+      break;
+
+    case 'b':
+      blocksize=(unsigned int) atoi(optarg);
+      break;
+
+    default:
+      return 0;
+
+    }
+  }
+
+  // Open input file
+  infile=fopen(infname,"r");
+  
+  // Check if input file exists
+  if (infile==NULL) {
+    fprintf(stderr,"Error opening %s\n",infname);
+    exit;
+  }
+
+  // Open output file
+  outfile=fopen(outfname,"w");
+  
+  // Check if input file exists
+  if (outfile==NULL) {
+    fprintf(stderr,"Error opening %s\n",outfname);
+    exit;
+  }
+
+  // Read header
+  hdr=read_dada_header(infile);
+
+  // Write header struct
+  fwrite(&hdr,sizeof(struct dada_header),1,outfile);
+
+  // Allocate buffer
+  buffer=(char *) malloc(sizeof(char)*blocksize);
+
+  // Iterate over file
+  for (;;) {
+    // Read buffer
+    bytes_read=fread(buffer,sizeof(char),blocksize,infile);
+
+    // Write buffer
+    fwrite(buffer,sizeof(char),bytes_read,outfile);
+
+    // End if no bytes are read
+    if (bytes_read==0)
+      break;
+  }
+
+  // Close output file
+  fclose(outfile);
+
+  // Close input file
+  fclose(infile);
+
+  // Free buffer
+  free(buffer);
+
+
+  return 0;
+}
