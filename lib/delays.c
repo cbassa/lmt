975baee072fa64f87883ee5d5b5ffed432ffe0b4
rsmits <rjm.smits@gmail.com>
2014-02-25 14:26:19 +0000

    continuing including delays

--- lib/delays.c
+++ lib/delays.c
@@ -19,7 +19,7 @@ FILE* Sopen(char *Fin, char *how)
 // The first time this is called, set SetCountCalcSteps to 1. This will then determine how far into the calc file to read.
 // The next times this routine is called, CountCalcSteps should be increased by 1 before calling and SetCountCalcSteps should be 0
 // Skipbins contains the skipbins from the input + skipbins to skip to the starttime of the last telescope + clockdelays
-// Samptime is in ns
+// Samptime is in seconds
 void ReadCalcfile(calc_type *Calc, double StartMJD, long SkipBins, double SampTime, int SetCountCalcSteps)
 {
   int i, j, MJDint, MJDsec;
@@ -39,7 +39,7 @@ void ReadCalcfile(calc_type *Calc, double StartMJD, long SkipBins, double SampTi
   Calc->CalcMJD = MJDint + MJDsec/86400.;
   // Calc offset (double) between start of calc and start of correlations in seconds with respect to reference telescope
   if (SetCountCalcSteps) {
-    Calc->Offset = (long) ((StartMJD - Calc->CalcMJD)*86400 + 0.5) + SkipBins*SampTime/1.E9;
+    Calc->Offset = (long) ((StartMJD - Calc->CalcMJD)*86400 + 0.5) + SkipBins*SampTime;
     Calc->CountCalcSteps = (int)(Calc->Offset/CALCSTEPSIZE);
   }
   // Read polynomials. First for reftel.
@@ -98,8 +98,8 @@ void ReadGPSFiles(gps_type *GPS, double StartMJD)
     if (line[0] != '#') {
       sscanf(line, "%f %f", &MJD, &ClockOffset_read);
       if (StartMJD < MJD) {
-	ClockOffset_Ref = (ClockOffset_read-ClockOffset_read_prev) * (StartMJD-MJD_prev)/(MJD-MJD_prev) + ClockOffset_read_prev;
-	ClockDrift_Ref += (ClockOffset_read - ClockOffset_read_prev)/86400; // in us / s
+	ClockOffset_Ref = (ClockOffset_read-ClockOffset_read_prev) * (StartMJD-MJD_prev)/(MJD-MJD_prev) + ClockOffset_read_prev; // in us, gets converted to s
+	ClockDrift_Ref += (ClockOffset_read - ClockOffset_read_prev)/86400; // in us / s, gets converted to s / s
 	Done = 1;
       }
       MJD_prev = MJD;
@@ -114,4 +114,103 @@ void ReadGPSFiles(gps_type *GPS, double StartMJD)
   // Move reference of clockoffset and clockdrift to telescope 0, so that telescope 0 has no offset and no drift
   GPS->ClockOffset -= ClockOffset_Ref;
   GPS->ClockDrift -= ClockDrift_Ref;
+  // Convert to seconds
+  GPS->ClockOffset /= 1e6; // in s
+  GPS->ClockDrift /= 1e6; // in s/s
+}
+
+// Rotate complex vector. Angle is in radians.
+void RotateVec(float *re, float *im, float angle)
+{
+  float x, y, cosangle, sinangle;
+  x = *re;
+  y = *im;
+  cosangle = cos(angle);
+  sinangle = sin(angle);
+  *re = x*cosangle - y*sinangle;
+  *im = x*sinangle + y*cosangle;
+}
+
+// This routine gets the geometrical offset of the start of the dada file. This is applied as soon as the files are opened.
+/// returns timedelay in seconds
+double GetStartGeoOffset(calc_type *Calc, double samptime, long reducedbinnumber)
+{
+  int i;
+  long binoffset;
+  double timedelay = 0;
+  for (i=0; i<NPOLYS; i++) {
+    timedelay += pow(reducedbinnumber*samptime, i) * Calc->Poly[i]; // in seconds. Gives timedelay from 0 to now
+  }
+  return -timedelay;
+}
+
+// Calculate the full geometric timedelay of one Telescope in units of s
+// reducedbinnumber is the binnumber from the start of the current calc-step to the current bin in units of output bins
+double GetTimedelay(delays_type *delays, calc_type *Calc, double samptime, long reducedbinnumber)
+{
+  int i;
+  double timedelay = 0;
+  for (i=0; i<NPOLYS; i++) {
+    timedelay += pow(reducedbinnumber*samptime, i) * Calc->Poly[i]; // in s. Gives timedelay from 0 to now
+  }
+  timedelay += delays->geoshifted*samptime; // subtract bins that are already shifted
+  return timedelay; // in s
+}
+
+// Calculate the fringerotation
+// time is the time from the start of the calcfile to now in seconds
+// timedelay is in s
+/*
+double GetFringeRotation(int N, delays_type delays, struct timeseries hdr, int freqcounter, double timedelay, calc_type *Calc, hdrdouble time, double samptime, int iscomplex, int Pol)
+{
+  double FringeRotation, ReducedFringeRotation, fracdelay, freqoffset;
+  if (Pol==0) {
+    fracdelay = delays->frac_delay_pol1;   // in s / BW
+    freqoffset = delays->phase_offset_pol1; // in radians
+  }
+  else {
+    fracdelay = delays->frac_delay_pol2;   // in s / BW
+    freqoffset = delays->phase_offset_pol2; // in radians
+  }
+  
+  // Correct for Fringerotation due to binshifting the clockoffset and geometric delay
+  if (iscomplex)
+    FringeRotation = -2*PI*(delays->BinsShifted - (timedelay+fracdelay)/samptime) 
+      * (Fileinfo->Freqinfo[Telescope].LowerFreqBand[0] + Fileinfo->Freqinfo[Telescope].BW/2) / Fileinfo->Freqinfo[Telescope].BW;
+  else
+    FringeRotation = -2*PI*(Fileinfo->BinsShifted[Telescope] - (timedelay+fracdelay)*1000/Fileinfo->SampTime[Telescope]) 
+      * (Fileinfo->Freqinfo[Telescope].LowerFreqBand[0]) / Fileinfo->Freqinfo[Telescope].BW;
+  
+  // Add correction for the Fractional Sample Error Correction + atmospheric delay
+  if (Fileinfo->IsComplex[Telescope])
+    FringeRotation += 2*PI*(timedelay+fracdelay)*((freqcounter-N/2)/(float)N*Fileinfo->BWCorr) + freqoffset - time * Fileinfo->FringeDrift[Telescope] - time * Fileinfo->ClockDrift[Telescope] * 
+      ((freqcounter-N/2)/(float)N*Fileinfo->BWCorr + Fileinfo->Freqinfo[Telescope].LowerFreqBand[0]) * 2*PI;
+  else
+    FringeRotation += 2*PI*(timedelay+fracdelay)*(freqcounter/(float)N*Fileinfo->BWCorr) + freqoffset - time * Fileinfo->FringeDrift[Telescope] - time * Fileinfo->ClockDrift[Telescope] * 
+      (freqcounter/(float)N*Fileinfo->BWCorr + Fileinfo->Freqinfo[Telescope].LowerFreqBand[0]) * 2*PI;
+  
+  return fmod(FringeRotation, 2*PI);
+}
+*/
+
+// Add delays from geometry to Skipbins, BinsShifted and GeoShifted
+// startgeooffset is in ns
+// I add 30 ms (given in MAXDELAYOFFSET) to make sure we never start with negative bins
+void Add_Geo_Delays(delays_type *delays, double startgeooffset, double samptime)
+{
+  int i;
+  long secondsdelay, BinShift;
+  if (startgeooffset < -MAXDELAYOFFSET*1e9) {fprintf(stderr, "ERROR, startgeooffset[%d] larger than %f ms!", i, MAXDELAYOFFSET*1e3); exit(0);}
+  BinShift = (long) (startgeooffset/samptime);
+  delays->skipbins += (long) (MAXDELAYOFFSET/samptime+DELTA) + BinShift; // add geometric delay to skipbins + 30ms
+  delays->binsshifted += BinShift;      // in bins
+  delays->geoshifted += BinShift;      // in bins
+}
+
+// Add delays due to the clockoffset
+void Add_Clock_Delays(delays_type *delay, gps_type *gps, double samptime)
+{
+  int i;
+  delay->skipbins += (long) (gps->ClockOffset/samptime); // add clock delay to skipbins
+  delay->binsshifted += (long) (gps->ClockOffset/samptime); // add clock delay to number of shifted bins
 }
--- reader.c
+++ reader.c
@@ -17,10 +17,12 @@ int main(int argc,char *argv[])
   char *buffer;
   calc_type calc;
   gps_type gps;
-  double startMJD, samptime;
-  long skipbins;
+  delays_type delays;
+  double startMJD, samptime, startgeooffset;
+  long skipbins, binnumber, reducedbinnumber;
   struct timeseries hdr;
   unsigned int bytes_read,blocksize=64000;
+  int iset=0, oset=0, bset=0, cset=0, gset=0, rset=0, sset=0;
 
   // Decode options
   while ((arg=getopt(argc,argv,"i:o:b:c:g:r:s:"))!=-1) {
@@ -28,38 +30,53 @@ int main(int argc,char *argv[])
       // Get input filename (dada file)
     case 'i':
       strcpy(infname,optarg);
+      iset = 1;
       break;
       // Get output filename (fifo)
     case 'o':
       strcpy(outfname,optarg);
+      oset = 1;
       break;
       // Get the blocksize
     case 'b':
       blocksize=(unsigned int) atoi(optarg);
+      bset = 1;
       break;
       // Get calc filename      
     case 'c':
       calc.filename = optarg;
+      cset = 1;
       break;
       // Get GPS filename of telescope
     case 'g':
       gps.filename = optarg;
+      gset = 1;
       break;
       // Get GPS filename of reference telescope
     case 'r':
       gps.filenameref = optarg;
+      rset = 1;
       break;
       // Get number of bins to shift
     case 's':
       skipbins = atoi(optarg);
       if (skipbins < 0) {fprintf(stderr, "Error, binshift cannot be negative\n");exit(0);}
+      sset = 1;
       break;
       
     default:
       return 0;
-
     }
   }
+  
+  // Check that all necessary parameters are set.
+  if (!iset){ fprintf(stderr, "Please provide input parameter with -i\n"); exit(0);}
+  if (!oset){ fprintf(stderr, "Please provide output parameter with -o\n"); exit(0);}
+  if (!bset){ fprintf(stderr, "Please provide blocksize with -b\n"); exit(0);}
+  if (!cset){ fprintf(stderr, "Please provide calcfilename with -c\n"); exit(0);}
+  if (!gset){ fprintf(stderr, "Please provide gps filename with -g\n"); exit(0);}
+  if (!rset){ fprintf(stderr, "Please provide reference gps filename with -r\n"); exit(0);}
+  if (!sset){ fprintf(stderr, "Please provide number of bins to shift with -s\n"); exit(0);}
 
   // Open input file
   infile=fopen(infname,"r");
@@ -91,7 +108,17 @@ int main(int argc,char *argv[])
   // Read Calc for geometric delay correction
   calc.Poly = (double*) calloc(NPOLYS, sizeof(double));
   ReadCalcfile(&calc, hdr.mjd_start, skipbins, hdr.tsamp, 1);
+  binnumber = (long)(calc.Offset/hdr.tsamp + 0.5); // binnumber from start of calc in output bins
+  reducedbinnumber = binnumber-(long)(calc.CountCalcSteps*CALCSTEPSIZE/hdr.tsamp); // binnumber from start of current Calc step
+  printf("binnumber: %e\n", binnumber);
+  printf("reducedbinnumber: %e\n", reducedbinnumber);
+  printf("samptime: %e\n", hdr.tsamp);
+  startgeooffset = GetStartGeoOffset(&calc, hdr.tsamp, reducedbinnumber);
+  printf("startgeooffset: %e\n");
+  Add_Geo_Delays(&delays, startgeooffset, hdr.tsamp);
+  Add_Clock_Delays(&delays, &gps, hdr.tsamp);
 
+  exit(0);
   // Print information
   printf("Reader: %s with %s at %s\n",hdr.source,hdr.instrument,hdr.telescope);
   printf("Reader: %s timeseries, %g us sampling, %d polarizations, %d bits\n",(hdr.ndim==1 ? "real" : "complex"),hdr.tsamp*1e6,hdr.npol,hdr.nbit);
